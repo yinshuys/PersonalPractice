571cc4b6905f5ec8854c1e8c75b7e0627eadb5ec
yinshuys <2313021413@qq.com>
2023-12-12 11:33:06 +0800

    20231212

---
+++ src/main/java/meituan/mathes/happyNum.java
@@ -0,0 +1,40 @@
+package meituan.mathes;
+
+/**
+ * 编写一个算法来判断一个数 n 是不是快乐数。
+ * <p>
+ * 「快乐数」 定义为：
+ * <p>
+ * 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+ * 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+ * 如果这个过程 结果为 1，那么这个数就是快乐数。
+ * 如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
+ */
+public class happyNum {
+    /**
+     * 快慢指针
+     * 视为链表ListNode
+     *
+     * @param n
+     * @return
+     */
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = getNext(n);
+        while (fast != slow && fast != 1) {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        }
+        return fast == 1;
+    }
+
+    public int getNext(int n) {
+        int ans = 0;
+        while (n > 0) {
+            int d = n % 10;
+            ans += d * d;
+            n = n / 10;
+        }
+        return ans;
+    }
+}
